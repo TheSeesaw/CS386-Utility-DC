64fc96681ed42c68ba37691da2bec1b20c99231f
Seesaw <seesaw.schindele@gmail.com>
2017-04-19 20:16:49 -0700

    Add comment to Runner.java.

--- Runner.java
+++ Runner.java
@@ -5,6 +5,7 @@ public class Runner
 {
   public static void main( String[] args ) throws IOException
   {
+     // Check if the users.txt file exists or not
     File userFile = new File( "users.txt" );
     UserManagement users = new UserManagement( userFile );
     LoginScreen login = new LoginScreen( users );
